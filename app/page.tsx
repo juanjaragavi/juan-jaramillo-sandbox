66a4f8ff5b850b78e82969a7e2565a01dadcef62
juanjaragavi <info@juanjaramillo.tech>
2023-10-14 22:56:23 -0500

    Before last checking

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="league-spartan pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://juanjaramillo.tech"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Go to <span className="font-mono font-bold text-pink-500">juanjaramillo.tech</span>
+            Go to <span className="text-pink-500">juanjaramillo.tech</span>
           </a>
         </div>
       </div>
