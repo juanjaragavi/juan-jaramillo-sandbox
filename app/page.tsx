0ea9524a94b0aa479d7a2fe7d856064b96a57b90
Juan Jaramillo <juanamillo@proton.me>
2024-03-26 12:05:35 -0500

    Home updated

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="logo-juan-jaramillo-blanco.webp"
+          src="https://juan-jaramillo-ai-assets.s3.amazonaws.com/logo-juan-jaramillo-blanco.webp"
           alt="Juan Jaramillo | Prompt Engineer · Developer · AI and Machine Learning Expert"
           width={230}
           height={47}
@@ -33,19 +33,19 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="/zione-chatbot/"
+          href="https://ai.juanjaramillo.tech"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-pink-500 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            ZIONE Chatbot{" "}
+            Chatbot as a Service{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            LLaMA2 70B powered advanced customer service chatbot by ZIONE Shop.
+            Custom Chatbot Builder platform filled with powerful features.
           </p>
         </a>
 
