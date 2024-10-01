9ad044db0ce62f1b9e0b575b9f2352224a56185d
Juan Jaramillo <info@juanjaramillo.tech>
2024-10-01 17:44:18 -0500

    Latest changes

--- app/page.tsx
+++ app/page.tsx
@@ -102,6 +102,11 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <iframe 
+    src="https://www.openassistantgpt.io/embed/cm1r0rkoq0003za8ziqgizud9/window?chatbox=false"
+    className="overflow: hidden; height: 80vh; width: 480px; bottom: -30px; border: 2px solid #e2e8f0; border-radius: 0.375rem; box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);" allow="clipboard-read; clipboard-write" 
+    >
+    </iframe>
     </main>
   );
 }
