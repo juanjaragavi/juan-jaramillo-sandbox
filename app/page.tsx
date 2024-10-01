e71a0e8943f507bbd7464aca3653fc3dde5d19d7
Juan Jaramillo <info@juanjaramillo.tech>
2024-10-01 17:41:52 -0500

    Latest changes
    Latest commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+  const fechaActual = new Date();
+  const opciones = { year: 'numeric', month: 'long', day: 'numeric' };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -21,14 +23,12 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/logo-juan-jaramillo-blanco.webp"
-          alt="Juan Jaramillo | Prompt Engineer · Developer · AI and Machine Learning Expert"
-          width={230}
-          height={47}
-          priority
-        />
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="text-center p-5 shadow-lg rounded-lg bg-white">
+        <h1 className="text-xl font-bold">Fecha Actual</h1>
+        <p>{fechaActual.toLocaleDateString('es-ES', opciones)}</p>
+      </div>
+    </div>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
